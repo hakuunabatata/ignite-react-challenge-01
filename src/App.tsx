40b01073ffc8395bcd64fa0c03e9faf553e7acac
hakuunabatata <emachadomartins1999@gmail.com>
2024-01-16 20:15:50 -0300

    create basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<string[]>(['aaa'])
+  const [value, setValue] = useState('')
+
+  const addTodo = () => {
+    if (!todos.includes(value)) {
+      setTodos(prev => [...prev, value])
+      setValue('')
+    }
+  }
+
+  const removeTodo = (index: number) => {
+    setTodos(prev => prev.filter((_, idx) => idx !== index))
+  }
 
   return (
     <>
+      <input
+        type='text'
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <button
+        onClick={addTodo}
+        disabled={!value || todos.includes(value)}
+      >
+        Add
+      </button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {todos.map((todo, index) => (
+          <>
+            <div key={index}>{todo}</div>
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
